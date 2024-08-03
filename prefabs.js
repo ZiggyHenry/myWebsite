9c9c40be1761c58fd7755a3076a19c8e6196aa70
ZigZogger <ziggycooperhenry@gmail.com>
2024-08-02 17:59:23 -0700

    banner good for now

--- prefabs.js
+++ prefabs.js
@@ -1,28 +1,32 @@
 class Banner extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `         
-            <div id="banner" style="position: absolute; left: 30px; top:30px">
-                <img src="banner.jpg" height="100px">
-                <a href="home.html">
-                <img src="home.jpg" height="70px" style="position: absolute; 
-                    left: 10px; top: 17px;">
-                </a>
-                <a href="pictures.html">
-                <h1 class="text" style="position: absolute; left: 100px; top: 10px;">
-                    Pictures</h1>
-                </a>
-                <a href="songs.html">
-                <h1 class="text" style="position: absolute; left: 230px; top: 10px;">
-                    Songs</h1>
-                </a>
-                <a href="stories.html">
-                    <h1 class="text" style="position: absolute; left: 330px; top: 10px;">
-                        Stories</h1>
-                </a>
-                <a href="film.html">
-                    <h1 class="text" style="position: absolute; left: 440px; top: 10px;">
-                        film</h1>
-                </a>
+            <div style="position: absolute; left: 30px; top:30px">
+            <img src="banner.jpg" height="100px" style="position:absolute; z-index: 0;">
+                <div style="position: relative; left:10px; top:-10px; z-index:20;">
+                    <a class="banner" href="home.html" style="position:relative; top:26px">
+                    <img src="home.jpg" height="70px">
+                    </a>
+                    
+                    <a class="btext" href="pictures.html">
+                    <h1 class="text">
+                        Pictures</h1>
+                    </a>
+                    <a class="btext" href="songs.html">
+                    <h1 class="text">
+                        Songs</h1>
+                    </a>
+                    <a class="btext" href="stories.html">
+                        <h1 class="text">
+                            Stories</h1>
+                    </a>
+                    <a class="btext" href="film.html">
+                        <h1 class="text">
+                            film</h1>
+                    </a>
+                    
+                </div>
+                
             </div>
       `;
     }
