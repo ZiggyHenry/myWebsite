0172148bd93c54aec16cadeb4e50f085cb929643
ZigZogger <ziggycooperhenry@gmail.com>
2024-08-05 19:45:43 -0700

    song page down (aside from decriptions)

--- prefabs.js
+++ prefabs.js
@@ -10,10 +10,10 @@ class Banner extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
             <div style="position: absolute; left: 30px; top:30px; white-space: nowrap;">
-            <img src="banner.jpg" height="100px" style="position:absolute; z-index: 0;">
+            <img src="images/banner.jpg" height="100px" style="position:absolute; z-index: 0;">
                 <div style="position: relative; left:10px; top:-10px; z-index:20;">
                     <a class="banner" href="home.html" style="position:relative; top:26px">
-                        <img src="home.jpg" height="70px">
+                        <img src="images/home.jpg" height="70px">
                         <p style="text-decoration: none; display: none;
                         ">Home!</p>
                     </a>
@@ -55,7 +55,7 @@ class boilerHead extends HTMLElement {
       this.innerHTML = `
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="icon" type="image/jpg" href="home.jpg">
+        <link rel="icon" type="image/jpg" href="images/home.jpg">
       `;
     }
 }
